46393c3eeac1ef12ec8f4fd83afb6cca2f42c7cf
Daniil Trukhin <truxin.daniil@gmail.com>
2026-01-09 18:28:41 +0500

    parsed 'ftyp' and 'mdat' into structures

--- main.go
+++ main.go
@@ -3,14 +3,42 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
+	"errors"
 	_ "unicode"
 	_ "unicode/utf8"
 
 	"os"
 )
 
-func chopBytes(content *[]byte, amount uint) []byte {
+const fccSize uint = 4
+const minAtomSize int = 8
+
+type fourCC = [fccSize]byte
+
+type Ftyp struct {
+	Size             uint32 // A 32-bit integer that specifies the number of bytes in the atom.
+	Type             fourCC // A 32-bit unsigned integer that identifies the atom type, typically represented as a four-character code.
+	MajorBrand       fourCC // A 32-bit unsigned integer that represents a file format code.
+	MinorVersion     uint32 // A 32-bit field that indicates the file format specification version.
+	CompatibleBrands fourCC // A series of unsigned 32-bit integers listing compatible file formats.
+}
+
+type Mdat struct {
+	Size         int32  // A 32-bit integer that specifies the number of bytes in the atom.
+	Type         fourCC // A 32-bit unsigned integer that identifies the atom type, typically represented as a four-character code.
+	ExtendedSize int64  // A 64-bit integer that specifies the number of bytes in this media data atom.
+	Data         []byte // content
+}
+
+func copyBytes(content *[]byte, amount uint32) []byte {
+    res := make([]byte, amount)
+	copy(res, (*content)[:amount])
+    return res
+}
+
+func chopBytes(content *[]byte, amount uint64) []byte {
     val := (*content)[0:amount]
     (*content) = (*content)[amount:]
     return val
@@ -35,6 +63,53 @@ func chopUint32(content *[]byte, order binary.ByteOrder) uint32 {
     return u32
 }
 
+func getFtyp(atomData []byte) (Ftyp, error) {
+	var ftyp Ftyp
+	err := binary.Read(bytes.NewReader(atomData), binary.BigEndian, &ftyp)
+	if err != nil {
+		return ftyp, err
+	}
+	return ftyp, nil
+}
+
+func getMdat(data []byte) (Mdat, error) {
+	const aHeaderFieldsSize = 16
+
+	var mdat Mdat
+	if len(data) < aHeaderFieldsSize {
+		return mdat, errors.New("too low amount of bytes received!")
+	}
+
+	var size int32
+	sizeBytes := copyBytes(&data, 4)
+	err := binary.Read(bytes.NewReader(sizeBytes), binary.BigEndian, &size)
+	if err != nil {
+		return mdat, err
+	}
+
+	aType := [4]byte(data[4:8])
+
+	var extendedSize int64
+	err = binary.Read(bytes.NewReader(data[8:aHeaderFieldsSize]), binary.BigEndian, &extendedSize)
+	if err != nil {
+		return mdat, err
+	}
+	extendedSize = max(extendedSize, 0)
+
+	aSize := int64(size)
+	if aSize == 1 {
+		aSize = extendedSize
+	}
+	aData := data[aHeaderFieldsSize:aSize]
+	mdat = Mdat{
+		Size: size,
+		Type: aType,
+		ExtendedSize: extendedSize,
+		Data: aData,
+	}
+	return mdat, nil
+}
+
 func main() {
     path := "manul.mov"
     content, err := os.ReadFile(path)
@@ -43,15 +118,34 @@ func main() {
     }
 
     fmt.Printf("Total bytes count: %v\n", len(content))
+    for len(content) >= minAtomSize {
+		var skipSize uint64
+		var size uint32
+        defaultSizeBytes := copyBytes(&content, 4)
+        binary.Read(bytes.NewReader(defaultSizeBytes), binary.BigEndian, &size)
+		skipSize = uint64(size)
 
-    for len(content) > 0 {
-        var sizeU32 uint32
+		withoutSize := content[4:]
+		atype := copyBytes(&withoutSize, 4)
+		fmt.Printf("Atom size: %d, type: %s\n", size, atype)
 
-        size := chopFourCC(&content)
-        binary.Read(bytes.NewReader(size), binary.BigEndian, &sizeU32)
-        atype := chopFourCC(&content)
-		fmt.Printf("Atom size: %d, type: %s\n", sizeU32, atype)
+		strAType := string(atype)
+		if strAType == "ftyp" { // || strAType == "wide" 
+			ftyp, err := getFtyp(content[:skipSize])
+			if err != nil { panic(err) }
+			ftypJson, err := json.MarshalIndent(ftyp, "", "  ")
+			fmt.Printf("  ftyp: %v\n", string(ftypJson))
+		} else if strAType == "mdat" {
+			mdat, err := getMdat(content)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("  mdat s: %v, t: %s, es: %v, len d: %v\n", mdat.Size, mdat.Type, mdat.ExtendedSize, len(mdat.Data))
+			if skipSize == 1 {
+				skipSize = uint64(mdat.ExtendedSize)
+			}
+		}
 
-		_ = chopBytes(&content, uint(sizeU32) - 8)
+		_ = chopBytes(&content, skipSize)
     }
 }
